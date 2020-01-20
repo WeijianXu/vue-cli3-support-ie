4125488c1cf79d1469b0d88f0dc4c0b3740aa4b3
wjxu <weijian.xuwj@01bees.com>
2020-01-20 16:52:17 +0800

    feat: 支持elementUI按需加载

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,7 @@ module.exports = {
   presets: [
     // web 和 移动端均可访问
     ['@vue/app', {
-      // 标环境不需要这些多填充，则会自动排除这些多的填充。
+      // 目标环境不需要这些多填充，则会自动排除这些多的填充。
       useBuiltIns: 'usage',
       /* polyfills: [
         'es6.promise',
@@ -20,6 +20,13 @@ module.exports = {
       {
         libraryName: 'view-design',
         libraryDirectory: 'src/components',
+      },
+    ],
+    [
+      'component',
+      {
+        libraryName: 'element-ui',
+        styleLibraryName: 'theme-chalk',
       },
     ],
   ],
